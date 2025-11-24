536db058f29c6a75302b21c7d555d3ac8740fd30
Subhashish Nabajja <94594955+subhashish-clg@users.noreply.github.com>
2025-11-24 19:07:40 +0530

    Remove unnecessary text from search drawer

--- src/components/common/search-drawer.tsx
+++ src/components/common/search-drawer.tsx
@@ -85,7 +85,6 @@ export default function SearchDrawer({ isOpen, onClose }: any) {
                   className="block"
                 >
                   <div className="flex gap-6">
-                    os
                     <div className="w-28 h-36 overflow-hidden">
                       <img
                         className="w-full h-full object-cover"
