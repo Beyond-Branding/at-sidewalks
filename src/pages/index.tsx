2a20b40b94cf098a02b4143a4c6d371af7918c0c
Abin Pillai <pillaiabincs232414@gmail.com>
2026-01-08 20:56:38 +0530

    chore: removed coming soon page
    removed coming soon page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
-import { useEffect, useState } from "react";
-
+import Layout from "@/components/common/layout";
 import { HeroCard } from "@/components/common/hero-card";
 import { LastestBlogs } from "@/components/home/latest-blogs";
 import { ShopReadAndDiscover } from "@/components/home/shop-and-discover";
 import { MustReadStories } from "@/components/home/must-read-stories";
-import Layout from "@/components/common/layout";
 
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { getCommonData } from "@/controllers/common.controller";
 import { getPosts, getStickyPosts } from "@/controllers/posts.controller";
-
-import ComingSoonPage from "./comingsoon";
-
-const LAUNCH_TIME = new Date(2026, 0, 8, 21, 0, 0).getTime();
 
 export const getStaticProps = (async () => {
   const { footer } = await getCommonData();
@@ -38,24 +32,6 @@ export const getStaticProps = (async () => {
 export default function HomePage(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-  const [isLive, setIsLive] = useState(false);
-
-  useEffect(() => {
-    const checkLaunch = () => {
-      setIsLive(Date.now() >= LAUNCH_TIME);
-    };
-
-    checkLaunch();
-
-    const interval = setInterval(checkLaunch, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  if (!isLive) {
-    return <ComingSoonPage />;
-  }
-
   return (
     <Layout footer={props.footer}>
       <div>
